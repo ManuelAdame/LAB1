fbb9aa5c0f46e1c6d6a92f5fd7730a8c65e09750
Manuel Adame <manoloadame60@gmail.com>
2025-03-05 20:26:59 +0100

    Lab 1 finalizado

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,92 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
 
+      name:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      description:{
+        type: Sequelize.STRING
+      },
+
+      address:{
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+
+      postalCode:{
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+
+      url:{
+        type: Sequelize.STRING
+      },
+
+      shippingCosts:{
+        allowNull:false,
+        type: Sequelize.DOUBLE
+      },
+
+      averageServiceMinutes:{
+        type: Sequelize.DOUBLE
+      },
+
+      email:{
+        type: Sequelize.STRING
+      },
+
+      phone:{
+        type: Sequelize.STRING
+      },
+
+      logo:{
+        type: Sequelize.STRING
+      },
+
+      heroImage:{
+        type: Sequelize.STRING
+      },
+
+      status:{
+        allowNull:false,
+        type: Sequelize.ENUM("online", "offline", "closed", "temporarily closed")
+      },
+
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
